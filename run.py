f570e269d4a43ea35105d89cf8ec6e47c0f201ce
demon119 <demon119@163.com>
2019-09-10 16:32:49 +0800

    修改data绝对路径

--- run.py
+++ run.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     report_path = Conf.get_report_path()+os.sep+"result"
     report_html_path = Conf.get_report_path()+os.sep+"html"
     pytest.main(["-s","--alluredir",report_path])
-    Base.send_mail()
+    #Base.send_mail()
 
 
 
